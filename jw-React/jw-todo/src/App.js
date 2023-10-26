001bd3fa1f019f69a22c73b3c1e722b1d673f894
chvgf <chvgf@naver.com>
2023-10-26 21:34:27 +0900

    todo 시작

---
+++ jw-React/jw-todo/src/App.js
@@ -0,0 +1,32 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { styled } from "styled-components";
+import MainPage from "./components/page/MainPage";
+
+const MainTitle = styled.div`
+  background-color: #d2e3ed;
+  width: 200px;
+  height: 40px;
+  border-radius: 20px;
+  font-size: 24px;
+  font-weight: bold;
+  text-align: center;
+  line-height: 40px;
+  position: absolute;
+  right: 0;
+  bottom: 0;
+`;
+
+function App() {
+  return (
+    <BrowserRouter>
+      <MainTitle>준우 todo</MainTitle>
+      <Routes>
+        <Route path="/" element={<MainPage />}/>
+        <Route/>
+        <Route/>
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default App;
